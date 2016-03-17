1c5f731e5f7df86a4d5f2d6fe7aefb1e1d9fe875
Nathan <reymer_n@hotmail.com>
2016-03-17 01:33:05 -0400

    50%

--- CandySearch/AboutView.swift
+++ CandySearch/AboutView.swift
@@ -10,7 +10,9 @@ import Foundation
 import UIKit
 
 class AboutView: UIViewController {
+    @IBOutlet weak var vLabel: UILabel!
    
+    @IBOutlet weak var bLabel: UILabel!
     @IBAction func webButton(sender: AnyObject) {
 
             UIApplication.sharedApplication().openURL(NSURL(string: "http://reymern.wix.com/pulseplus")!)
