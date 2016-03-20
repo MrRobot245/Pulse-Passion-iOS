debd3340a11c08b248acf322b317eab860bca3a7
Nathan <reymer_n@hotmail.com>
2016-03-19 21:08:24 -0400

    Cancer Looking

---
+++ CandySearch/FeaturedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeaturedView.swift
+//  MyPulse
+//
+//  Created by Nathan Reymer on 2016-03-19.
+//
+//
+
+import Foundation
+import UIKit
+class FeaturedView: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
