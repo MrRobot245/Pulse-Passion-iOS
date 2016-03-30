a38109f70d8b489734cef4086a33edf552ec1a0e
Nathan <reymer_n@hotmail.com>
2016-03-30 02:30:41 -0400

    Bigger

---
+++ CandySearch/LearnMore.swift
@@ -0,0 +1,28 @@
+//
+//  LearnMore.swift
+//  MyPulse
+//
+//  Created by Nathan Reymer on 2016-03-30.
+//
+//
+
+import Foundation
+import UIKit
+
+
+class LearnMore: UIViewController
+{
+    @IBOutlet weak var HDTView: UITextView!
+    override func viewDidLoad()
+    {
+      //  self.HDTView.setContentOffset(CGPointZero, animated: false)
+        
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
