a0777d055c0d0de371facaed95ce6a4863430018
Nathan Reymer <reymer_n@hotmail.com>
2016-07-07 14:36:04 -0400

    Flashlight Almost there

--- CandySearch/ScanViewController.swift
+++ CandySearch/ScanViewController.swift
@@ -8,26 +8,81 @@
 
 import UIKit
 import RSBarcodes
+import AVFoundation
 
 class ScanViewController: RSCodeReaderViewController {
+    var barcode: String = ""
+    var dispatched: Bool = false
+    
+    @IBOutlet weak var flashLight: UIButton!
+    
+    @IBAction func toggle(sender: AnyObject?) {
+        let isTorchOn = self.toggleTorch()
+        print(isTorchOn)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         self.focusMarkLayer.strokeColor = UIColor.whiteColor().CGColor
-        
         self.cornersLayer.strokeColor = UIColor.greenColor().CGColor
+        for subview in self.view.subviews {
+            self.view.bringSubviewToFront(subview)
+        }
+
+//        
+        // MARK: NOTE: If you layout views in storyboard, you should these 3 lines
+        for subview in self.view.subviews {
+            self.view.bringSubviewToFront(subview)
+        }
         
+        if !self.hasTorch() {
+            self.flashLight.enabled = false
+        }
         
-        self.barcodesHandler = { barcodes in
-            
-            for barcode in barcodes {
-                
-                
-                print("Barcode found: type=" + barcode.type + " value=" + barcode.stringValue)
+        let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        if (device.hasTorch) {
+            do {
+                try device.lockForConfiguration()
+                if (device.torchMode == AVCaptureTorchMode.On) {
+                    device.torchMode = AVCaptureTorchMode.Off
+                } else {
+                    try device.setTorchModeOnWithLevel(1.0)
+                }
+                device.unlockForConfiguration()
+            } catch {
+                print(error)
             }
         }
+        
+        self.barcodesHandler = { barcodes in
+            if !self.dispatched { // triggers for only once
+                self.dispatched = true
+                for barcode in barcodes {
+                    self.barcode = barcode.stringValue
+                    print("Barcode found: type=" + barcode.type + " value=" + barcode.stringValue)
+                    
+                    let alert = UIAlertController(title: "Hey-o!", message:barcode.stringValue, preferredStyle: .Alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .Default) { _ in })
+                    self.presentViewController(alert, animated: true){}
+                    
+//                    dispatch_async(dispatch_get_main_queue(), {
+//                        self.performSegueWithIdentifier("nextView", sender: self)
+//                        
+//                        // MARK: NOTE: Perform UI related actions here.
+//                    })
+                }
+            }
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.dispatched = false // reset the flag so user can do another scan
+        
+        super.viewWillAppear(animated)
+        
+//        self.navigationController?.navigationBarHidden = true
+        
     }
 
 }
