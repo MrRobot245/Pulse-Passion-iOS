02b3b31fbe8e6c11749cd5a9178bdb8530f161b3
Nathan Reymer <reymer_n@hotmail.com>
2016-07-29 19:52:18 -0400

    Embeddeded views is fun

--- CandySearch/MasterViewController.swift
+++ CandySearch/MasterViewController.swift
@@ -11,39 +11,12 @@ class MasterViewController: UITableViewController {
     let searchController = UISearchController(searchResultsController: nil)
     var filteredFood = [Food]()
     var detailViewController: DetailViewController? = nil
-    var food = [Food]()
     
-    //FMDB
-    func fmdb(){
-        
-        let documents = try! NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: false)
-        let fileURL = documents.URLByAppendingPathComponent("DB2.sqlite")
-        let database = FMDatabase(path: fileURL.path)
-        if !database.open() {
-            print("Unable to open database")
-            return
-        }
-        
-        let querySQL = "SELECT cat,title,fRate,iList,gList,bList,iRate FROM DB WHERE title = title ORDER BY title COLLATE NOCASE"
-        let results:FMResultSet? = database.executeQuery(querySQL,
-                                                         withArgumentsInArray: nil)
-        while(results!.next()) {
-            //print("\(results!.stringForColumn("cat"),results!.stringForColumn("food"))")
-            food.append(Food(category: results!.stringForColumn("cat"),
-                name: results!.stringForColumn("title"),
-                fRate: results!.stringForColumn("fRate"),
-                iList: results!.stringForColumn("iList"),
-                gList: results!.stringForColumn("gList"),
-                bList: results!.stringForColumn("bList"),
-                iRate: results!.stringForColumn("iRate")))
+    var getFood = [Food]()
 
-        }
-        tableView.reloadData()
-        
-    }
-    override func viewDidLoad() {
-        copyDatabase()
-        fmdb()
+
+      override func viewDidLoad() {
+     
         self.searchController.loadViewIfNeeded()
         //self.resultSearchController.loadViewIfNeeded()
     }
@@ -56,23 +29,14 @@ class MasterViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    func copyDatabase(){
-        let fileManger = NSFileManager.defaultManager()
-        let doumentDirectoryPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
-        let destinationPath = doumentDirectoryPath.stringByAppendingPathComponent("DB2.sqlite")
-        let sourcePath = NSBundle.mainBundle().pathForResource("DB2", ofType: "sqlite")
-        if (fileManger.fileExistsAtPath(destinationPath)) {
-            try!fileManger.removeItemAtPath(destinationPath)
-        }
-        try! fileManger.copyItemAtPath(sourcePath!, toPath: destinationPath)
-    }
+
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return food.count
+        return getFood.count
     }
     
     
@@ -82,7 +46,7 @@ class MasterViewController: UITableViewController {
         if searchController.active && searchController.searchBar.text != "" {
             candy = filteredFood[indexPath.row]
         } else {
-            candy = food[indexPath.row]
+            candy = getFood[indexPath.row]
         }
         cell.textLabel?.text = candy.name
         cell.detailTextLabel?.text = candy.category
@@ -97,7 +61,7 @@ class MasterViewController: UITableViewController {
                 if searchController.active && searchController.searchBar.text != "" {
                     candy = filteredFood[indexPath.row]
                 } else {
-                    candy = food[indexPath.row]
+                    candy = getFood[indexPath.row]
                 }
                 let controller = (segue.destinationViewController as! DetailViewController)
                 controller.detailCandy = candy
@@ -107,7 +71,7 @@ class MasterViewController: UITableViewController {
         }
         if segue.identifier == "sendAllBar" {
             let dbTran = segue.destinationViewController as! ScanViewController
-            dbTran.getFood = food
+            dbTran.getFood = getFood
         }
     }
 }
--- CandySearch/SearchMain.swift
+++ CandySearch/SearchMain.swift
@@ -21,9 +21,74 @@ class SearchMain: UIViewController {
     }
     @IBOutlet weak var searchField: UITextField!
     
+    var food = [Food]()
     
+    //FMDB
+    func fmdb(){
+        
+        let documents = try! NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: false)
+        let fileURL = documents.URLByAppendingPathComponent("DB2.sqlite")
+        let database = FMDatabase(path: fileURL.path)
+        if !database.open() {
+            print("Unable to open database")
+            return
+        }
+        
+        let querySQL = "SELECT cat,title,fRate,iList,gList,bList,iRate FROM DB WHERE title = title ORDER BY title COLLATE NOCASE"
+        let results:FMResultSet? = database.executeQuery(querySQL,
+                                                         withArgumentsInArray: nil)
+        while(results!.next()) {
+            //print("\(results!.stringForColumn("cat"),results!.stringForColumn("food"))")
+            food.append(Food(category: results!.stringForColumn("cat"),
+                name: results!.stringForColumn("title"),
+                fRate: results!.stringForColumn("fRate"),
+                iList: results!.stringForColumn("iList"),
+                gList: results!.stringForColumn("gList"),
+                bList: results!.stringForColumn("bList"),
+                iRate: results!.stringForColumn("iRate")))
+            
+        }
+    }
+    func copyDatabase(){
+        let fileManger = NSFileManager.defaultManager()
+        let doumentDirectoryPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
+        let destinationPath = doumentDirectoryPath.stringByAppendingPathComponent("DB2.sqlite")
+        let sourcePath = NSBundle.mainBundle().pathForResource("DB2", ofType: "sqlite")
+        if (fileManger.fileExistsAtPath(destinationPath)) {
+            try!fileManger.removeItemAtPath(destinationPath)
+        }
+        try! fileManger.copyItemAtPath(sourcePath!, toPath: destinationPath)
+    }
+
         override func viewDidLoad() {
+            copyDatabase()
+            fmdb()
 
     }
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+         if (segue.identifier == "sendToTable") {
+        
+        let vc: UINavigationController = segue.destinationViewController as! UINavigationController
+        let detailVC = vc.topViewController as! MasterViewController
+        detailVC.getFood = food
+        }
+        
+//        
+//        if (segue.identifier == "sendToTable") {
+//            let dest = segue.destinationViewController as! MasterViewController
+//            dest.getFood = food
+//            //Assign to dest's properties
+//        }
+    }
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+//        
+//        if segue.identifier == "sendToTable" {
+//            let vc = segue.destinationViewController as! MasterViewController
+//            vc.getFood = food
+//        }
+//        
+//        
+//    }
+//    
 }
