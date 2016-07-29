107ed902b6dcef106e5186b61be85c2c6cc27061
Nathan Reymer <reymer_n@hotmail.com>
2016-07-29 16:07:22 -0400

    Google Like search Wire

---
+++ CandySearch/SearchMain.swift
@@ -0,0 +1,22 @@
+//
+//  SearchMain.swift
+//  MyPulse
+//
+//  Created by Nathan Reymer on 2016-07-29.
+//
+//
+
+
+
+import Foundation
+import UIKit
+
+class SearchMain: UIViewController {
+    @IBAction func unwindToMainSearch(segue: UIStoryboardSegue) {
+    }
+    
+        override func viewDidLoad() {
+
+    }
+    
+}
