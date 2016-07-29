22e0b6b5997bdaea8571dc6724d4550b233c21e4
Nathan Reymer <reymer_n@hotmail.com>
2016-07-29 17:02:51 -0400

    Loads half assed

--- CandySearch/MasterViewController.swift
+++ CandySearch/MasterViewController.swift
@@ -1,24 +1,4 @@
-/*
- * Copyright (c) 2015 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+
 
 import UIKit
 import RSBarcodes
@@ -76,48 +56,8 @@ class MasterViewController: UITableViewController {
         copyDatabase()
         fmdb()
         super.viewDidLoad()
-        searchController.searchResultsUpdater = self
-        searchController.dimsBackgroundDuringPresentation = false
-        definesPresentationContext = true
-        //self.navigationItem.titleView = searchController.searchBar ;
-        tableView.tableHeaderView = searchController.searchBar
-        searchController.searchBar.scopeButtonTitles = ["All", "Limit", "Sometimes", "Often"]
-        //searchController.searchBar.scopeButtonTitles = ["All", "Red", "Yellow", "Green"]
-        searchController.searchBar.delegate = self
     }
-    
-    func filterContentForSearchText(searchText: String, scope: String = "All") {
-        
-        let searchTerms = searchText.componentsSeparatedByString(" ").filter { $0 != "" }
-        
-        filteredFood = food.filter { candy in
-            let categoryMatch = (scope == "All") || (candy.fRate == scope)
-            if searchTerms == [] && !categoryMatch {
-                return false
-            }
-            for term in searchTerms{
-                if !candy.name.lowercaseString.containsString(term.lowercaseString) {
-                    return false
-                }
-                if !categoryMatch {
-                    return false
-                }
-                
-            }
-            
-            return true
-        }
-        tableView.reloadData()
-    }
-    //    func filterContentForScope(scope: String = "All") {
-    //        filteredFood = food.filter { candy in
-    //            let categoryMatch = (scope == "All") || (candy.fRate == scope)
-    //            return  categoryMatch
-    //        }
-    //        tableView.reloadData()
-    //    }
-    //
-    //
+
     override func viewWillAppear(animated: Bool) {
         // clearsSelectionOnViewWillAppear = splitViewController!.collapsed
         super.viewWillAppear(animated)
@@ -142,9 +82,6 @@ class MasterViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if searchController.active && searchController.searchBar.text != "" {
-            return filteredFood.count
-        }
         return food.count
     }
     
@@ -193,15 +130,15 @@ class MasterViewController: UITableViewController {
 
 extension MasterViewController: UISearchResultsUpdating {
     func updateSearchResultsForSearchController(searchController: UISearchController) {
-        let searchBar = searchController.searchBar
-        let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
-        filterContentForSearchText(searchController.searchBar.text!, scope: scope)
+        //let searchBar = searchController.searchBar
+       // let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+       // filterContentForSearchText(searchController.searchBar.text!, scope: scope)
     }
 }
 extension MasterViewController: UISearchBarDelegate {
     func searchBar(searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
         //filterContentForScope(searchBar.scopeButtonTitles![selectedScope])
-        filterContentForSearchText(searchBar.text!, scope: searchBar.scopeButtonTitles![selectedScope])
+       // filterContentForSearchText(searchBar.text!, scope: searchBar.scopeButtonTitles![selectedScope])
         
     }
 }
